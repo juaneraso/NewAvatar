b05240b2a4eda7f51233a08273809b11bbe2ae27
juaneraso <juangui9508@hotmail.com>
2024-10-16 13:59:25 -0500

    first commit

---
+++ src/components/UI.jsx
@@ -0,0 +1,120 @@
+import { useRef,useState } from "react";
+import { useChat } from "../hooks/useChat";
+
+export const UI = ({ hidden, ...props }) => {
+  const input = useRef();
+  const { chat, loading, cameraZoomed, setCameraZoomed, message } = useChat();
+
+
+  const sendMessage = () => {
+    const text = input.current.value;
+    if (!loading && !message) {
+      chat(text);
+      input.current.value = "";
+    }
+  };
+  if (hidden) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 bottom-0 z-10 flex justify-between p-4 flex-col pointer-events-none">
+        {/* <div className="self-end backdrop-blur-md bg-white bg-opacity-50 p-4 rounded-lg text-right max-w-md">
+          <h1 className="font-black text-center">ALICE</h1>
+          <p className="text-justify">🌟 Soy Alice, el nuevo avatar de la cámara de comercio, 
+                quien te guiara en todo lo que necesites 
+                saber sobre emprendimiento y muchas cosas🌟</p>
+        </div> */}
+
+          
+        <div className="w-full flex flex-col items-end justify-center gap-4">
+          
+          <button hidden
+            onClick={() => setCameraZoomed(!cameraZoomed)}
+            className="pointer-events-auto bg-camara hover:bg-camara2 text-white p-4 rounded-md"
+          >
+            {cameraZoomed ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM13.5 10.5h-6"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6"
+                />
+              </svg>
+            )}
+          </button>
+          <button hidden
+            onClick={() => {
+              const body = document.querySelector("body");
+              if (body.classList.contains("greenScreen")) {
+                body.classList.remove("greenScreen");
+              } else {
+                body.classList.add("greenScreen");
+              }
+            }}
+            className="pointer-events-auto bg-camara hover:bg-camara2 text-white p-4 rounded-md"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
+              />
+            </svg>
+          </button>
+        </div>
+        
+        <div className="flex items-center gap-2 pointer-events-auto max-w-screen-sm w-full mx-auto">
+          <input
+            className="w-full placeholder:text-midnight placeholder:italic p-4 rounded-md bg-opacity-50 bg-white backdrop-blur-md"
+            placeholder="Escribe tu mensaje..."
+            ref={input}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage();
+              }
+            }}
+          />
+          <button 
+            disabled={loading || message}
+            onClick={sendMessage}
+            className={`bg-camara hover:bg-camara2 text-white p-4 px-10 font-semibold uppercase rounded-md ${
+              loading || message ? "cursor-not-allowed opacity-30" : ""
+            }`}
+          >
+            Enviar
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'camara': '#24195d',
+      'camara2': '#342483',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
+  plugins: [],
+};
