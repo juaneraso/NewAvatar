f2b3b06ca6f1196e5ceb4c5bec0dd2d6cd3e37d1
juaneraso <juangui9508@hotmail.com>
2024-10-28 09:34:58 -0500

    connect to back

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-//const backendUrl = "http://localhost:3000";
+//const backendUrl = "http://localhost:3003";
 
 const backendUrl = import.meta.env.VITE_APP_BACK;
 
